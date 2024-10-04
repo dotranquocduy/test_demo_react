cbaba22ed1ba2dadecd7ac86a44fea708ed36d2c
doduy <dotranquocduy2000@gmail.com>
2024-10-04 15:49:22 +0700

    update react bai 1

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,9 +1,11 @@
 // class component
 // function component
 
+import { eventWrapper } from "@testing-library/user-event/dist/utils";
 import React from "react";
-class MyComponent extends React.Component {
+ class MyComponent extends React.Component {
 
+    
     state ={
         name: 'duy',
         address:'55/27 nguyen phuc chu',
@@ -25,16 +27,33 @@ class MyComponent extends React.Component {
       handleMouseOver = (event) => {
         console.log(event.pageX);
       }
+      handleOnChange = (event) =>{
+        this.setState({
+           name :event.target.value
+        })
+     
+      }
+      handleOnSubmit =(event) =>{
+        event.preventDefault(); // ngan chan load lai trang 
+        console.log(this.state);
+      }
 
-
+   
    //jsx => chuyen doi viết code javascript bên trong code html
     render() {
         return (
             <div>
                 My first my component
                <br></br> My name : {this.state.name} and {this.state.age}
-               <button onClick={ (event) =>{this.handleClick(event)} }>click me</button>
-               <button onMouseOver={this.handleMouseOver}>hover me</button>
+              
+                <form onSubmit={(event)=>{ this.handleOnSubmit(event) }}>
+                    <input type="text"
+                    onChange={(event)=>{
+                        this.handleOnChange(event)
+                    }}
+                    />
+                    <button>submit</button>
+                </form>
             </div>
            
         );
