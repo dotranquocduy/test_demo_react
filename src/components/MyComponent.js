c2fd896b00deac27b3664dfb4232b122b317298e
doduy <dotranquocduy2000@gmail.com>
2024-10-04 13:27:47 +0700

    baitap react-tt

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,15 +3,19 @@
 
 import React from "react";
 class MyComponent extends React.Component {
+
+    state ={
+        name: 'duy',
+        address:'55/27 nguyen phuc chu',
+        age: 24
+      };
    
    //jsx => chuyen doi viết code javascript bên trong code html
     render() {
         return (
             <div>
                 My first my component
-                {
-                    Math.random()
-                }
+               <br></br> My name : {this.state["name"]} and {this.state.address}
             </div>
            
         );
