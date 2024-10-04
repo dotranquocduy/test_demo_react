88b02e0cdd53f3fa3e78f0bb3e41c92353dc94b7
doduy <dotranquocduy2000@gmail.com>
2024-10-04 15:01:20 +0700

    baitap1-react-update

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -9,13 +9,32 @@ class MyComponent extends React.Component {
         address:'55/27 nguyen phuc chu',
         age: 24
       };
+      
+      handleClick = (event) => {
+        console.log(">>> click me my button")
+        
+        // merge state  => react class
+
+        this.setState({
+            name :'duytran',
+            age:  Math.floor((Math.random()*100)+1)
+        })
+    }
+       
    
+      handleMouseOver = (event) => {
+        console.log(event.pageX);
+      }
+
+
    //jsx => chuyen doi viết code javascript bên trong code html
     render() {
         return (
             <div>
                 My first my component
-               <br></br> My name : {this.state["name"]} and {this.state.address}
+               <br></br> My name : {this.state.name} and {this.state.age}
+               <button onClick={ (event) =>{this.handleClick(event)} }>click me</button>
+               <button onMouseOver={this.handleMouseOver}>hover me</button>
             </div>
            
         );
